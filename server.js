b132fe3324f68d00e0d0c1ba64e329d8e53576c9
ABHINANDAN MAITY <git@pixelsolutionz.in>
2024-07-11 21:15:26 +0530

    change in Title

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const PORT=process.env.PORT||3000
 
 app.get('/', function (req, res) {
   res.send('Hello World From Abhinandan')
 })
 
-app.listen(3000)
+app.listen(PORT)
