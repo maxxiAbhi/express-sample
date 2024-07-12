343c1bfce3e7adbc7c1517527961200366862266
ABHINANDAN MAITY <git@pixelsolutionz.in>
2024-07-12 09:06:14 +0530

    working on azure app service

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT=process.env.PORT||3000
 
 app.get('/', function (req, res) {
-  res.send('Hello World From Abhinandan')
+  res.send('Hello World From Abhinandan Yooooooooooooooooooooooooooooooooooooooooooooooo')
 })
 
 app.listen(PORT)
